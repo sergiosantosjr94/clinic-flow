7ee14a7314d7659cfc667c8a97f4630cc28e6d5c
sergiosantosjr94 <sllsjunior@gmail.com>
2025-06-08 21:25:21 -0300

    feat: add Radix UI dialog component and update package dependencies

--- src/app/(protected)/clinic-form/page.tsx
+++ src/app/(protected)/clinic-form/page.tsx
@@ -4,23 +4,22 @@ import {
   DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
+} from "@/components/ui/dialog";
 
 const ClinicFormPage = () => {
   return (
-   <Dialog>
-  <Dialog>Open</Dialog>
-  <DialogContent>
-    <DialogHeader>
-      <DialogTitle>Are you absolutely sure?</DialogTitle>
-      <DialogDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
-      </DialogDescription>
-    </DialogHeader>
-  </DialogContent>
-</Dialog>
+    <Dialog>
+      <Dialog>Open</Dialog>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   );
 };
 
